e0374d7f93a76275a68ed1fdbcab1dd2d64ef959
Dipre <clement.dipre.etu@univ-lille.fr>
2025-03-20 16:30:26 +0100

    added abstract play engine state

--- common/src/main/java/org/red5/server/stream/state/AbstractPlayEngineState.java
+++ common/src/main/java/org/red5/server/stream/state/AbstractPlayEngineState.java
@@ -1,13 +1,17 @@
 package org.red5.server.stream.state;
 
+import org.red5.server.api.scope.IScope;
+import org.red5.server.api.stream.IPlayItem;
+import org.red5.server.messaging.IMessageInput;
 import org.red5.server.stream.PlayEngine;
-
+import org.red5.server.stream.message.RTMPMessage;
+import org.slf4j.Logger;
 import java.io.IOException;
 
 public abstract class AbstractPlayEngineState {
 
     private PlayEngine playEngine;
-
+    protected Logger log;
     public PlayEngine getPlayEngine() {
         return playEngine;
     }
@@ -16,5 +20,7 @@ public abstract class AbstractPlayEngineState {
         this.playEngine = playEngine;
     }
 
-    public abstract void play() throws IOException;
+    public abstract void play(IPlayItem playItem, IMessageInput in, IScope thisScope, boolean withReset) throws IOException;
+
+    public abstract void sendMessage(RTMPMessage messageIn);
 }
